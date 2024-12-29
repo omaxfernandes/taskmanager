cd35f3adbba983eef271ed5ccf02eb7d04fc928a
omaxfernandes <max.sfernandes@outlook.com>
2024-12-29 00:24:41 -0300

    feat: create a user_table using orm jpa

--- src/main/java/br/com/omaxfernandess/taskmanager/user/UserModel.java
+++ src/main/java/br/com/omaxfernandess/taskmanager/user/UserModel.java
@@ -1,11 +1,26 @@
 package br.com.omaxfernandess.taskmanager.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "users_table")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String name;
     private String password;
     private String email;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
