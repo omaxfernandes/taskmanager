8af9716c4c28edc4a9ec845bcde6057dde9bd95b
omaxfernandes <max.sfernandes@outlook.com>
2024-12-29 00:35:56 -0300

    feat: create role for register user, on banco de dados

--- src/main/java/br/com/omaxfernandess/taskmanager/user/UserController.java
+++ src/main/java/br/com/omaxfernandess/taskmanager/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.omaxfernandess.taskmanager.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println("User: " + userModel.getName() + " created with success " );
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
